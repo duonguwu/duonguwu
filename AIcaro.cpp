f2a540ad8e926ad1f54de8d3be8163f83efad80f
Nguyễn Dương <97208260+duonguwu@users.noreply.github.com>
2022-05-17 21:59:10 +0700

    Caro

---
+++ AIcaro.cpp
@@ -0,0 +1,167 @@
+#include "TicTacToe.cpp"
+
+struct AIMove
+{
+    AIMove(){};
+    AIMove(int Score) : score(Score){};
+    int index;
+    int score;
+};
+
+AIMove getBestMove(vector<int> &GameBoard, int player)
+{
+    SquareState rv = checkVictory(GameBoard);
+    if (rv == O_VAL)
+        return AIMove(10);
+    else if (rv == X_VAL)
+        return AIMove(-10);
+    else if (rv == TIE_VAL)
+        return AIMove(0);
+
+    vector<AIMove> moves; // Khai báo mảng moves
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (GameBoard[i] == NO_VAL) 
+        {
+            AIMove items;
+            items.index = i;
+            GameBoard[i] = player;
+
+            if (player == X_VAL)
+                items.score = getBestMove(GameBoard, O_VAL).score;
+            else if (player == O_VAL)
+                items.score = getBestMove(GameBoard, X_VAL).score;
+
+            moves.push_back(items); // Thêm giá trị vào mảng moves để tính đường đi
+            GameBoard[i] = NO_VAL;  // Trả về ô trống cho vị trí vừa xét
+        }
+    }
+    // Đoạn này là tính toán đưa ra đường đi tốt nhất.
+    int bestMove = 0;
+    if (player == O_VAL)  
+    {
+        int bestScore = INT_MIN;
+        int size = moves.size();
+        for (int i = 0; i < size; i++)
+        {
+            if (moves[i].score > bestScore)
+            {   
+                bestMove = i;
+                bestScore = moves[i].score;
+            }
+        }
+    }
+    else if (player == X_VAL)
+    {
+        int bestScore = INT_MAX;
+        int size = moves.size();
+        for (int i = 0; i < size; i++)
+        {
+            if (moves[i].score < bestScore)
+            {
+                bestMove = i;
+                bestScore = moves[i].score;
+            }
+        }
+    }
+    return moves[bestMove];
+}
+
+// Nếu mình đánh vào 1 trong 4 góc, index sẽ trả về 4 và đánh ô chính giữa
+// => không thể chiến thắng được ;')))
+// Còn nếu mình đánh vào giữa trước thì index trả về 0, đánh vào vị trí góc đầu tiên. 
+// Sau đó sẽ dựa vào điều kiện để đánh chặn mình dẫn đến hòa
+
+void performMove(vector<int> &gameBoard)
+{
+    AIMove bestMove = getBestMove(gameBoard, O_VAL);
+    gameBoard[bestMove.index] = O_VAL;
+}
+
+void playWithAI()
+{
+    system("cls");
+    vector<int> gb;
+    Input(gb);
+    char x;
+    do
+    {   
+            system("cls");
+            cout << "---|----|----\n";
+            cout << " " << (char)gb[0] << " | " << (char)gb[1] << "  | " << (char)gb[2]  << "   \n";
+            cout << "___|____|____\n";
+            cout << "   |    |   \n";
+            cout << " " << (char)gb[3] << " | " << (char)gb[4] << "  | " <<  (char)gb[5] << "   \n";
+            cout << "___|____|____\n";
+            cout << "   |    |   \n";
+            cout << " " << (char)gb[6] << " | " <<  (char)gb[7] <<"  | " <<   (char)gb[8] << "  \n";
+            cout << "___|____|____\n";
+            cout << "   |    |   \n"; 
+            
+        player(gb, "I");
+
+        if (checkVictory(gb) == X_VAL)  // Này cho có thôi chứ win kiểu gì đc =))))))
+        {   
+            cout << "You win !!!";
+            return;
+        }
+        if (isFull(gb))     // Hết chỗ rồi mà chưa win thì hòa
+        {   
+            cout << "Ties !!!";
+            break;
+        }
+
+        performMove(gb);
+        //badMove(gb);
+        if (checkVictory(gb) == O_VAL)
+        {   
+            cout << "You lose!!! Ga vl :')))";
+            break;
+        }
+    } while (checkVictory(gb) == NO_VAL);
+}
+
+void HuongDan(){
+    cout << "Ta co ban co voi 9 vi tri: ";
+    cout << "\n\t1 2 3 \n\t4 5 6 \n\t7 8 9" << endl; 
+    cout << "Su dung cac nut Q W E A S D Z X C tuong ung voi cac chi so."<< endl;
+
+}
+
+void Menu(int &x){
+    //system("cls");
+    cout << "\n============ Menu ============" << endl;
+    cout << "\n\t1. Play";
+    cout << "\n\t2. How to play?";
+    cout << "\n\t0. Exit" << endl;
+    cout << "\n==============================" << endl;
+    cout << "Moi nhap lua chon: ";
+    cin >> x;
+}
+
+int main(){
+
+    int x;
+    int k;
+    do
+    {   
+        Menu(x); 
+        switch (x)
+        {
+        case 1:
+            playWithAI();
+            break;
+        case 2:
+            system("cls");
+            HuongDan();
+            break;
+        default:
+            break;
+        }
+
+    } while (x != 0);
+
+    
+
+}
---
+++ TicTacToe.cpp
@@ -0,0 +1,158 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+enum SquareState
+{
+    NO_VAL = ' ',
+    X_VAL = 'X',
+    O_VAL = 'O',
+    TIE_VAL
+};
+
+struct Game
+{
+    const int width;
+    const int height;
+    int GameBoard[9];
+};
+
+void Input(vector<int> &GameBoard){
+    for(int i = 0; i < 9; i++){
+        GameBoard.push_back(NO_VAL);    // Thêm các phần tử trống vào bàn cờ
+    }
+}
+
+void setX(vector<int> &GameBoard, int value)    // Thêm điểm X
+{
+	GameBoard[value] = X_VAL;
+} 
+
+void setO(vector<int> &GameBoard, int value)    // Thêm điểm O 
+{
+	GameBoard[value] = O_VAL;
+} 
+
+bool isTaken(vector<int> GameBoard, int value)  // Hàm kiểm tra ô trống
+{
+	return GameBoard[value] == NO_VAL;  // Trả về true nếu ô value trống
+}
+
+bool isFull(vector<int> GameBoard)  // Kiểm tra bàn cờ đã đầy chưa
+{
+	for (int i = 0; i < GameBoard.size(); i++)
+	{
+		if (GameBoard[i] == NO_VAL) // Kiểm tra xem bàn cờ còn ô trống hay ko
+			return false;
+	}
+	return true;
+}
+
+// Hàm kiểm tra điều kiện chiến thăngs
+
+
+SquareState checkVictory(vector<int> &GameBoard)
+{
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+
+    // Trường hợp thằng hàng nằm ngang
+
+	if (GameBoard[0] == X_VAL && GameBoard[1] == X_VAL && GameBoard[2] == X_VAL)
+		return X_VAL;
+	if (GameBoard[0] == O_VAL && GameBoard[1] == O_VAL && GameBoard[2] == O_VAL)
+		return O_VAL;
+	if (GameBoard[3] == X_VAL && GameBoard[4] == X_VAL && GameBoard[5] == X_VAL)
+		return X_VAL;
+	if (GameBoard[3] == O_VAL && GameBoard[4] == O_VAL && GameBoard[5] == O_VAL)
+		return O_VAL;
+	if (GameBoard[6] == X_VAL && GameBoard[7] == X_VAL && GameBoard[8] == X_VAL)
+		return X_VAL;
+	if (GameBoard[6] == O_VAL && GameBoard[7] == O_VAL && GameBoard[8] == O_VAL)
+		return O_VAL;
+
+
+    // Trường hợp thẳng hàng nằm dọc
+
+	if (GameBoard[0] == X_VAL && GameBoard[3] == X_VAL && GameBoard[6] == X_VAL)
+		return X_VAL;
+	if (GameBoard[0] == O_VAL && GameBoard[3] == O_VAL && GameBoard[6] == O_VAL)
+		return O_VAL;
+	if (GameBoard[1] == X_VAL && GameBoard[4] == X_VAL && GameBoard[7] == X_VAL)
+		return X_VAL;
+	if (GameBoard[1] == O_VAL && GameBoard[4] == O_VAL && GameBoard[7] == O_VAL)
+		return O_VAL;
+	if (GameBoard[2] == X_VAL && GameBoard[5] == X_VAL && GameBoard[8] == X_VAL)
+		return X_VAL;
+	if (GameBoard[2] == O_VAL && GameBoard[5] == O_VAL && GameBoard[8] == O_VAL)
+		return O_VAL;
+
+    // Trường hợp đường chéo
+
+	if (GameBoard[0] == X_VAL && GameBoard[4] == X_VAL && GameBoard[8] == X_VAL)
+		return X_VAL;
+	if (GameBoard[0] == O_VAL && GameBoard[4] == O_VAL && GameBoard[8] == O_VAL)
+		return O_VAL;
+	if (GameBoard[2] == X_VAL && GameBoard[4] == X_VAL && GameBoard[6] == X_VAL)
+		return X_VAL;
+	if (GameBoard[2] == O_VAL && GameBoard[4] == O_VAL && GameBoard[6] == O_VAL)
+		return O_VAL;
+
+	for (int i = 0; i < 9; i++)
+	{
+		if(GameBoard[i] == NO_VAL)
+			return NO_VAL;
+	}
+	return TIE_VAL;
+}
+
+void player(vector<int> &GameBoard, string player)
+{
+	int value;
+	while(true)
+	{
+		if (1)
+		{
+			char key = getchar();
+
+			if (key == 27)// ESC => exit
+				exit(0);
+
+			// Chỗ này là mình bấm các phím trên bàn phím để đánh lên bàn cờ
+			// Với mỗi nút, sẽ có 1 cái value tương ứng. 
+
+			if (player.compare("I") == 0) // player 1
+			{
+				if ((key == 'Q' || key == 'q') && isTaken(GameBoard, 0))
+					{value = 0;  break; }
+				else if ((key == 'W' || key == 'w') && isTaken(GameBoard, 1)) // Kiểm tra xem ô có trống hay kh
+					{value = 1;  break; }
+				else if ((key == 'E' || key == 'e') && isTaken(GameBoard, 2))
+					{value = 2;  break; }
+				else if ((key == 'A' || key == 'a') && isTaken(GameBoard, 3))
+					{value = 3;  break; }
+				else if ((key == 'S' || key == 's') && isTaken(GameBoard, 4))
+					{value = 4;  break; }
+				else if ((key == 'D' || key == 'd') && isTaken(GameBoard, 5))
+					{value = 5;  break; }
+				else if ((key == 'Z' || key == 'z') && isTaken(GameBoard, 6))
+					{value = 6;  break; }
+				else if ((key == 'X' || key == 'x') && isTaken(GameBoard, 7))
+					{value = 7;  break; }
+				else if ((key == 'C' || key == 'c') && isTaken(GameBoard, 8))
+					{value = 8;  break; }
+			}
+		}
+	}
+	// Sau khi có cái value khi nhập từ bàn phím, mình sẽ đánh dấu X vào vị trí value trên bàn cờ
+
+    if (player.compare("I") == 0){ // player 1 
+		setX(GameBoard, value);
+	}
+	else if (player.compare("II") == 0){
+		setO(GameBoard, value);
+	}
+}
